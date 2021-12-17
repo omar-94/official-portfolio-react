88e056f51fb66fef335e2327a8b58a560fb1fe0a
onava <enava23@yahoo.com>
2021-12-16 22:32:38 -0500

    hamburger icon transform

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Intro from "./components/Intro/Intro";
 import Portfolio from "./components/Portfolio/Portfolio";
 import WebProjects from "./components/WebProjects/WebProjects";
 import Contact from "./components/Contact/Contact";
+import { useState } from "react";
 import "./app.scss"
 
 
 function App() {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <div className="app">
-      <Navbar />
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro />
         <Portfolio />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import "./navbar.scss";
 import { Person, Mail } from '@mui/icons-material';
 
-export default function NavBar() {
+export default function NavBar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="navbar">
+    <div className={"navbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href='#intro' className='logo'> nava.</a>
@@ -18,6 +18,11 @@ export default function NavBar() {
           </div>
         </div>
         <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+              <span className='line1'></span>
+              <span className='line2'></span>
+              <span className='line3'></span>
+          </div>
         </div>
       </div> 
     </div>
