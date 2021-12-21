62b5e2177d8d74713e3134d0a8b371a0d8b3d265
onava <enava23@yahoo.com>
2021-12-21 01:02:52 -0500

    portfolioData displayed per selection

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./portfolio.scss";
 import PortfolioList from '../PortfolioList/PortfolioList';
+import { webDevPorftolio, graphicDesignPortfolio, photographyPortfolio } from '../../portfolioData';
 
 export default function Portfolio() {
 
-  const [selected, setSelected] = useState('web-development');
+  const [selected, setSelected] = useState('web-development');    //useState to set current selection's id of submenu
+  const [data, setData] = useState([])       //useState to hold current data selected to be displayed... set up as empty array as different OBJECTS will be passed
   
-  const list = [
-    {
+  const list = [                //list of different portfolios
+    {                           //creates submenu in portfolio page  
       id: 'web-development',
       title: 'Web Development'
     },
@@ -24,6 +26,23 @@ export default function Portfolio() {
       title: 'Extra'
     }
   ]
+
+  useEffect(() => {
+    switch(selected){       //selected depending on below to see which from id list above is selected to match case and data
+      case 'web-development':
+        setData(webDevPorftolio);   //whichever id is selected... Data is set from portfolioData.js
+        break;
+      case 'graphic-design':
+        setData(graphicDesignPortfolio);
+        break;
+      case 'photography':
+        setData(photographyPortfolio);
+        break;   
+        default:
+          setData(webDevPorftolio)
+    }
+  }, [selected])        //dependency: when selected is change useEffect fires
+
   
   return (
     <div className="portfolio" id='portfolio'>
@@ -43,22 +62,12 @@ export default function Portfolio() {
         ))}
       </ul>
       <div className="container">
-        <div className='item'>
-          <img src="https://i.ytimg.com/vi/E1GDVMAwdN4/maxresdefault.jpg" alt="web ui" width="300" height="150" />
-          <h3>Web Application</h3>
-        </div>
-        <div className='item'>
-          <img src="https://i.ytimg.com/vi/E1GDVMAwdN4/maxresdefault.jpg" alt="web ui" width="300" height="150" />
-          <h3>Web Application</h3>
-        </div>
-        <div className='item'>
-          <img src="https://i.ytimg.com/vi/E1GDVMAwdN4/maxresdefault.jpg" alt="web ui" width="300" height="150" />
-          <h3>Web Application</h3>
-        </div>
-        <div className='item'>
-          <img src="https://i.ytimg.com/vi/E1GDVMAwdN4/maxresdefault.jpg" alt="web ui" width="300" height="150" />
-          <h3>Web Application</h3>
-        </div>
+        {data.map((dataDetails) => (                  //maps each object of data(from useState) and sets parameter as 'dataDetails'
+          <div className='item'>
+            <img src={dataDetails.img} alt="" width="300" height="150" />
+            <h3>{dataDetails.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
   )
---
+++ src/portfolioData.js
@@ -0,0 +1,74 @@
+export const webDevPorftolio = [
+  {
+    id: 1,
+    title: 'Social Media App',
+    img:
+    "https://i.ytimg.com/vi/E1GDVMAwdN4/maxresdefault.jpg"
+  },
+  {
+    id: 2,
+    title: 'Movie App',
+    img:
+    "https://www.sliderrevolution.com/wp-content/uploads/2020/10/spott.jpg"
+  },
+  {
+    id: 3,
+    title: 'E-Commerce',
+    img:
+    "https://designshack.net/wp-content/uploads/sketch-website-templates.jpg"
+  }
+];
+
+export const graphicDesignPortfolio = [
+  {
+    id: 1,
+    title: 'Northstar',
+    img:
+    "https://fortplatinum.com/wp-content/uploads/2021/05/Dizer-bootstrap-portfolio-template.jpg"
+  },
+  {
+    id: 2,
+    title: 'The Stranger',
+    img:
+    "https://i.ytimg.com/vi/E1GDVMAwdN4/maxresdefault.jpg"
+  },
+  {
+    id: 3,
+    title: 'A-1 Countertops',
+    img:
+    "https://i.pinimg.com/originals/4b/50/80/4b5080fc84948ed5b34806299cb38b9e.png"
+  },
+  {
+    id: 4,
+    title: 'RCAW',
+    img:
+    "https://designshack.net/wp-content/uploads/sketch-website-templates.jpg"
+  }
+];
+
+export const photographyPortfolio = [
+  {
+    id: 1,
+    title: 'Model A',
+    img:
+    "https://i.pinimg.com/originals/4b/50/80/4b5080fc84948ed5b34806299cb38b9e.png"
+  },
+  {
+    id: 2,
+    title: 'Landscape',
+    img:
+    "https://design.bookmobile.com/wp-content/uploads/2020/02/bookmobile-535-1024x683.jpg"
+  },
+  {
+    id: 3,
+    title: 'Graduation',
+    img:
+    "https://www.sliderrevolution.com/wp-content/uploads/2021/08/af.jpg"
+  },
+  {
+    id: 4,
+    title: 'Model B',
+    img:
+    "https://i.pinimg.com/originals/ed/de/b9/eddeb936e3c74afa5b0182f847fb59fd.png"
+  },
+];
